2acab5b2aab1c18a6b6861b60b3899569ba44465
Garv Virmani <131982472+GarvVirmani@users.noreply.github.com>
2025-04-29 10:52:01 +0530

    HomeInventory

--- Frontend/src/utils/constant.js
+++ Frontend/src/utils/constant.js
@@ -1,2 +1,2 @@
-export const USER_API_END_POINT="http://localhost:8080/user";
-export const ITEM_API_END_POINT="http://localhost:8080/item";
+export const USER_API_END_POINT="https://home-inventory-9zqa.onrender.com/user";
+export const ITEM_API_END_POINT="https://home-inventory-9zqa.onrender.com/item";
